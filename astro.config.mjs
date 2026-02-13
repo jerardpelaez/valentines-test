98caa9b7c4028556a43e0d9e5c22d2ab46a49c16
Jerard Pelaez <pelaezjerard@gmail.com>
2026-02-13 13:59:25 +0800

    pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jerardpelaez.github.io',
-  base: '/valentine',
+  base: '/valentines-test',
 });
